4c600af78212c71c6c12b6e2f5e5a60b104ec300
Miguel Luna <118997463+Miguelin04@users.noreply.github.com>
2023-06-20 02:52:15 -0500

    Lista Doblemente Circular

---
+++ Nodo.java
@@ -0,0 +1,35 @@
+public class Nodo {
+    private int valor;
+    private Nodo siguiente;
+    private Nodo anterior;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+}
